3189a15765c8b464ec99d700421ad3e7a7dced9f
zd3H03 <zderinco@gmail.com>
2020-03-18 15:32:50 +0900

    20200318 js,html.css

--- html/js/index.js
+++ html/js/index.js
@@ -20,3 +20,23 @@ $(function(){
   });
 });
 
+// header 最初から固定表示、特定位置までスクロールでサイズ・色を変える
+var _window = $(window),
+    _header = $('.glHeader'),
+    heroBottom;
+
+_window.on('scroll',function(){
+    heroBottom = $('.hero').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('transform');
+    }
+    else{
+        _header.removeClass('transform');
+    }
+});
+
+_window.trigger('scroll');
+
+
+
+
